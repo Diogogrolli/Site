9d09d03162d4c2495aae87b4f320d0cc44bc6e52
Diogogrolli <diogoyurigrolli@gmail.com>
2025-04-15 19:38:55 -0300

    mudança

---
+++ partejs.js
@@ -0,0 +1,18 @@
+
+
+    document.querySelectorAll("meu-botao").forEach(function(botao) {
+    botao.addEventListener("click", function() {
+        alert("BOTÃO CLICADO");
+    });
+});
+
+
+document.querySelectorAll("cadastro-botao").forEach(function(botao) {
+    botao.addEventListener("click", function() {
+        alert("Cadastro Feito");
+    });
+});
+
+
+
+
